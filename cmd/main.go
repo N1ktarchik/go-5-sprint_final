f7f0b745e3a7e2489c035b55476c54ae4a5b6bf7
Nikita Kleymenov <klejmenov663@gmail.com>
2026-01-16 05:00:48 +0300

    init

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
 func main() {
 
+	file, err := os.OpenFile("info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+	mylog := log.New(file, `serv `, log.LstdFlags|log.Lshortfile)
+
+	mylog.Println(`Start server`)
+	defer mylog.Println(`Finish server`)
+
+	server := server.MakeRouter(mylog)
+
+	err = http.ListenAndServe(server.Serv.Addr, server.Serv.Handler)
+	if err != nil {
+		mylog.Fatal(err)
+	}
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,34 @@
+// В этом пакете вы реализуете функцию автоматического определения кода Морзе или обычного текста из переданной строки.
+// Если передан обычный текст,функция должна переконвертировать его в код Морзе и вернуть;
+//  и наоборот — если был передан код Морзе, функция должна переконвертировать его в обычный текст и вернуть.
+// Для реализации этой функции придётся обратиться к стандартной библиотеке, а именно — к пакету strings.
+// В этом пакете есть хорошие примеры, которые демонстрируют, как можно решить эту задачу.
+// Не забудьте обработать ошибки и вернуть их.
+
 package service
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func AutoConvertor(reqStr string) (string, error) {
+
+	if len(reqStr) <= 0 {
+		err := errors.New("an empty string is not allowed.")
+		return "", err
+	}
+
+	processedStr := strings.ReplaceAll(reqStr, ".", "")
+	processedStr = strings.ReplaceAll(processedStr, "-", "")
+	processedStr = strings.ReplaceAll(processedStr, " ", "")
+
+	if len(processedStr) > 0 {
+		return morse.ToMorse(reqStr), nil
+	}
+
+	return morse.ToText(reqStr), nil
+
+}
